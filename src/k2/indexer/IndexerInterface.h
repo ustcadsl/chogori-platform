9eba42655ef96f2638ecec9e98e9b0f93341e8e0
wangshuil <40989395+wangshuil@users.noreply.github.com>
2021-06-02 12:33:05 +0800

    Add files via upload

--- src/k2/indexer/IndexerInterface.h
+++ src/k2/indexer/IndexerInterface.h
@@ -35,6 +35,250 @@ namespace k2
 //  K2 internal MVCC representation
 //
 
+class KeyValueNode
+{
+private:
+	uint64_t flags;
+	dto::DataRecord* keypointer;
+	typedef struct{
+		uint64_t timestamp;
+		dto::DataRecord* valuepointer;
+		} _valuedata;
+	_valuedata valuedata[3];//8type for ts and 8 byte for pointer
+
+public:
+	KeyValueNode() {
+		flags=0;
+		keypointer=0;
+		for(int i=0;i<2;i++)
+		{
+			valuedata[i].timestamp=0;
+			valuedata[i].valuepointer=0;
+		}
+	}
+	KeyValueNode(dto::Key key) {
+		flags=0;
+		keypointer=new dto::Key();
+		*keypointer=key;
+		for(int i=0;i<2;i++)
+		{
+			valuedata[i].timestamp=0;
+			valuedata[i].valuepointer=0;
+		}
+	}
+	~KeyValueNode() {
+		
+		delete keypointer;
+	}
+	dto::Key get_key(){
+		//assert(keypointer!=0);
+		return *keypointer;
+	}
+	inline bool _get_flag_i(int i){
+		return bool(flags & (1<<i));		
+	}
+	inline bool _set_flag_i(int i){
+		flags=flags | (1<<i);
+		return _get_flag_i(i);
+	}
+	inline bool _set_zero_flag_i(int i){
+		flags=flags & (~(1<<i));
+		return _get_flag_i(i);
+	}
+	inline bool is_writeintent(){
+		return _get_flag_i(63);
+	}
+	inline bool is_tombstone(int i){
+		return _get_flag_i(61-3i);
+	}
+	inline bool set_tombstone(int i, bool setas){
+		if(setas)
+			_set_flag_i(61-3i);
+		else 
+			_set_zero_flag_i(61-3i)
+		return _get_flag_i(61-3i);
+	}
+	inline bool is_inmem(int i){
+		return _get_flag_i(60-3i);
+	}
+	inline bool set_inmem(int i, bool setas){
+		if(setas)
+			_set_flag_i(60-3i);
+		else 
+			_set_zero_flag_i(60-3i)
+		return _get_flag_i(60-3i);
+	}
+	inline bool is_exist(int i){
+		return _get_flag_i(59-3i);
+	}
+	inline bool set_exist(int i, bool setas){
+		if(setas)
+			_set_flag_i(59-3i);
+		else 
+			_set_zero_flag_i(59-3i)
+		return _get_flag_i(59-3i);
+	}
+	int size(){
+		return (int)((flags<<32)>>32);
+	}
+	inline void size_inc(){
+		flags++;
+	}
+	int size_dec(){
+		if(flags==0) return 1;
+		flags--;
+		return 0;
+	}
+	void set_zero(int order){
+		valuedata[order].timestamp=0;
+		valuedata[order].valuepointer=0;
+	}
+	dto::DataRecord* get_datarecord(const dto::Timestamp& requesttimestamp){
+		for(int i=0;i<size();3++)
+			if(is_exist(i) && requesttimestamp>=valuedata[i].timestamp)
+				return valuedata[i].valuepointer;
+		dto::DataRecord* viter=valuedata[2].valuepointer;
+		while (viter != nullptr && requesttimestamp.compareCertain(viter->txnId.mtr.timestamp) < 0) {
+         // skip newer records
+        	viter=viter->prevVersion;
+   	 	}
+		return viter;
+	}
+	int insert_datarecord(dto::DataRecord* datarecord){
+		datarecord->prevVersion=valuedata[0].valuepointer;
+		for(int i=2;i>0;i--)
+		{
+			valuedata[i]=valuedata[i-1];
+			set_tombstone(i, is_tombstone(i-1));
+			set_exist(i, is_exist(i-1));
+			set_inmem(i, is_inmem(i-1));
+		}
+		valuedata[0].valuepointer=datarecord;
+		valuedata[0].timestamp=datarecord->txnId.mtr.timestamp.tEndTSECount();
+		size_inc();
+		set_tombstone(0,0);
+		set_exist(0,1);
+		set_inmem(0,0);
+		return 0;
+	}
+	int insert_hot_datarecord(const dto::Timestamp& timestamp,dto::DataRecord* datarecord){
+		for(int i=0;i<3;3++)
+			if(timestamp.tEndTSECount()==valuedata[i].timestamp)
+			{
+				set_inmem(i,1);
+				datarecord->prevVersion=valuedata[i].valuepointer;
+				valuedata[i].valuepointer=datarecord;
+				return 0;
+			}
+		return 1;
+	}
+	int evict_hot_datarecord(const dto::Timestamp& timestamp,dto::DataRecord* datarecord){
+		for(int i=0;i<3;3++)
+			if(timestamp.tEndTSECount()==valuedata[i].timestamp)
+			{
+				if(is_inmem(i) == false) return 1;
+				set_inmem(i,0);
+				valuedata[i].valuepointer=datarecord;
+				return 0;
+			}
+		return 1;
+	}
+	int remove_datarecord(const dto::Timestamp& timestamp){
+		dto::DataRecord* toremove;
+		for(int i=0;i<3;3++)
+			if(timestamp.tEndTSECount()==valuedata[i].timestamp)
+			{
+				toremove=valuedata[i].valuepointer;
+				if(i>0)
+				{
+					valuedata[i-1].valuepointer->prevVersion=valuedata[i].valuepointer->prevVersion;
+				}
+				for(int j=i;j<2;j++)
+				{
+					valuedata[j]=valuedata[j+1];
+					set_tombstone(j, is_tombstone(j+1));
+					set_exist(j, is_exist(j-1));
+					set_inmem(j, is_inmem(j-1));
+				}
+				set_zero(2);
+				set_tombstone(2,0);
+				set_exist(2,0);
+				set_inmem(2,0);
+				size_dec();
+				delete toremove;
+				return 0;
+			}
+		return 1;
+	}
+	int remove_datarecord(dto::DataRecord* datarecord){
+		dto::DataRecord* toremove;
+		for(int i=0;i<3;3++)
+			if(valuedata[i].valuepointer==datarecord){
+				this->remove_datarecord(i);
+				return 0;
+			}
+		dto::DataRecord* viter=valuedata[2].valuepointer;
+		while (viter->prevVersion != nullptr && viter->prevVersion != datarecord) {
+         // skip newer records
+        	viter=viter->prevVersion;
+   	 	}
+		if(viter->prevVersion == nullptr)
+			return 1;
+		toremove=viter->prevVersion;
+		viter->prevVersion=toremove->prevVersion;
+		size_dec();
+		delete toremove;
+		return 0;
+	}
+	int remove_datarecord(int order){
+		int i=order;
+		dto::DataRecord* toremove=valuedata[i].valuepointer;
+		if(i>0)
+		{
+			valuedata[i-1].valuepointer->prevVersion=valuedata[i].valuepointer->prevVersion;
+		}
+		for(int j=i;j<2;j++)
+		{
+			valuedata[j]=valuedata[j+1];
+			set_tombstone(j, is_tombstone(j+1));
+			set_exist(j, is_exist(j-1));
+			set_inmem(j, is_inmem(j-1));
+		}
+		set_zero(2);
+		set_tombstone(2,0);
+		set_exist(2,0);
+		set_inmem(2,0);
+		size_dec();
+		delete toremove;
+		return 0;
+	}
+	dto::DataRecord* _getpointer(int order){
+		return valuedata[order].valuepointer;
+	}
+	dto::DataRecord* begin()
+	{
+		return this->_getpointer(0);
+	}
+};
+
+class Indexer
+{
+
+public:
+    virtual KeyValueNode* insert(dto::Key key)=0;
+    virtual KeyValueNode* find(dto::Key &key)=0;
+    virtual KeyValueNode* begin()=0;
+    virtual KeyValueNode* end()=0;
+	virtual KeyValueNode* getiter()=0;
+	virtual KeyValueNode* beginiter()=0;
+	virtual KeyValueNode* setiter(dto::Key &key)=0;
+	virtual KeyValueNode* inciter()=0;
+
+    virtual void erase(dto::Key key)=0;
+    virtual size_t size()=0;
+};
+
+/*
 template<typename IndexerType, typename ValueType>
 class Indexer
 {
@@ -53,7 +297,7 @@ public:
 
 template<typename IndexerType, typename ValueType>
 inline typename IndexerType::iterator Indexer<IndexerType, ValueType>::insert(dto::Key key) {
-    auto ret = idx.insert(std::pair<dto::Key, std::deque<ValueType>>(key, std::deque<ValueType>()));
+    auto ret = idx.insert(std::pair<dto::Key, k2::KeyValueNode>(key, k2::KeyValueNode()));
     return ret.first;
 }
 
@@ -81,4 +325,8 @@ template<typename IndexerType, typename ValueType>
 inline size_t Indexer<IndexerType, ValueType>::size() {
     return idx.size();
 }
+*/
+
+
+
 }
--- src/k2/indexer/MapIndexer.h
+++ src/k2/indexer/MapIndexer.h
@@ -27,50 +27,129 @@ Copyright(c) 2020 Futurewei Cloud
 
 namespace k2
 {
+
+class mapindexer : public Indexer{
+private:
+    std::map<dto::Key, k2::KeyValueNode> idx;
+	std::map<dto::Key, k2::KeyValueNode>::iterator scanit;
+public: 
+	KeyValueNode* insert(dto::Key key);
+    KeyValueNode* find(dto::Key &key);
+    KeyValueNode* begin();
+    KeyValueNode* end();
+	KeyValueNode* getiter();
+	KeyValueNode* setiter(dto::Key &key);
+	KeyValueNode* beginiter();
+	KeyValueNode* inciter();
+
+    void erase(dto::Key key);
+    size_t size();
+};
+
+KeyValueNode* mapindexer::insert(dto::Key key)
+{
+	KeyValueNode newkvnode(key);
+	auto ret = idx.insert(std::pair<dto::Key , k2::KeyValueNode>(key, newkvnode));
+    return &(ret->first.second);
+}
+KeyValueNode* mapindexer::find(dto::Key& key)
+{
+	auto kit=idx.find(key);
+	if (kit==idx.end()) return nullptr;
+	return &(kit->second);
+}
+KeyValueNode* mapindexer::begin()
+{
+	auto kit=idx.begin();
+	return &(kit->second);
+}
+KeyValueNode* mapindexer::end()
+{
+	return nullptr;
+}
+KeyValueNode* mapindexer::getiter()
+{
+	if(scanit==idx.end()) return nullptr;
+	return &(scanit->second);
+}
+KeyValueNode* mapindexer::setiter(dto::Key &key)
+{
+	scanit=idx.find(key);
+	if (scanit==idx.end()) return nullptr;
+	return &(scanit->second);
+}
+KeyValueNode* mapindexer::beginiter()
+{
+	scanit==idx.begin();
+	if(scanit==idx.end()) return nullptr;
+	return &(scanit->second);
+}
+KeyValueNode* mapindexer::inciter()
+{
+	scanit++;
+	if(scanit==idx.end()) return nullptr;
+	return &(scanit->second);
+}
+void mapindexer::erase(dto::Key key)
+{
+	auto kit=idx.find(key);
+	if (kit==idx.end()) return;
+	idx.erase(kit);
+}
+
+inline size_t mapindexer::size()
+{
+	return idx.size();
+}
+
+
+
+/*
 template <typename ValueType>
-class Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>
+class Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>
 {
 private:
-    std::map<dto::Key, std::deque<ValueType>> idx;
+    std::map<dto::Key, k2::KeyValueNode> idx;
 
 public:
-    typename std::map<dto::Key, std::deque<ValueType>>::iterator insert(dto::Key key);
-    typename std::map<dto::Key, std::deque<ValueType>>::iterator find(dto::Key &key);
-    typename std::map<dto::Key, std::deque<ValueType>>::iterator begin();
-    typename std::map<dto::Key, std::deque<ValueType>>::iterator end();
+    typename std::map<dto::Key, k2::KeyValueNode>::iterator insert(dto::Key key);
+    typename std::map<dto::Key, k2::KeyValueNode>::iterator find(dto::Key &key);
+    typename std::map<dto::Key, k2::KeyValueNode>::iterator begin();
+    typename std::map<dto::Key, k2::KeyValueNode>::iterator end();
 
-    void erase(typename std::map<dto::Key, std::deque<ValueType>>::iterator it);
+    void erase(typename std::map<dto::Key, k2::KeyValueNode>::iterator it);
     size_t size();
 };
 
 template <typename ValueType>
-inline typename std::map<dto::Key, std::deque<ValueType>>::iterator Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>::insert(dto::Key key) {
-    auto ret = idx.insert(std::pair<dto::Key, std::deque<ValueType>>(key, std::deque<ValueType>()));
+inline typename std::map<dto::Key, k2::KeyValueNode>::iterator Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>::insert(dto::Key key) {
+    auto ret = idx.insert(std::pair<dto::Key , k2::KeyValueNode>(key, k2::KeyValueNode(key)));
     return ret.first;
 }
 
 template <typename ValueType>
-inline typename std::map<dto::Key, std::deque<ValueType>>::iterator Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>::find(dto::Key &key) {
+inline typename std::map<dto::Key, k2::KeyValueNode>::iterator Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>::find(dto::Key &key) {
     return idx.find(key);
 }
 
 template <typename ValueType>
-inline void Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>::erase(typename std::map<dto::Key, std::deque<ValueType>>::iterator it) {
+inline void Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>::erase(typename std::map<dto::Key, k2::KeyValueNode>::iterator it) {
     idx.erase(it);
 }
 
 template <typename ValueType>
-inline typename std::map<dto::Key, std::deque<ValueType>>::iterator Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>::begin() {
+inline typename std::map<dto::Key, k2::KeyValueNode>::iterator Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>::begin() {
     return idx.begin();
 }
 
 template <typename ValueType>
-inline typename std::map<dto::Key, std::deque<ValueType>>::iterator Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>::end() {
+inline typename std::map<dto::Key, k2::KeyValueNode>::iterator Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>::end() {
     return idx.end();
 }
 
 template <typename ValueType>
-inline size_t Indexer<std::map<dto::Key, std::deque<ValueType>>, ValueType>::size() {
+inline size_t Indexer<std::map<dto::Key, k2::KeyValueNode>, ValueType>::size() {
     return idx.size();
-}
+}*/
+
 }
