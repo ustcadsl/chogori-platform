9f2ac9eb7760767a58f8759dadb51b3d9179fe3d
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2021-12-30 19:03:17 +0800

    remove benchmark dependacy

--- install_deps.sh
+++ install_deps.sh
@@ -17,10 +17,3 @@ cd chogori-intervaltree
 mkdir -p build
 cd build
 cmake .. && make -j install
-
-if [ ! -d "benchmark"]; then git clone https://github.com/google/benchmark.git; fi
-git clone https://github.com/google/googletest.git benchmark/googletest
-cd benchmark
-cmake -E make_directory "build"
-cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
-cmake --build "build" --config Release --target install
